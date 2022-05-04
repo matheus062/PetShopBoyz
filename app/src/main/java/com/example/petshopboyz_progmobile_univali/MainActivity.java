6930291c65975ce5d54d1d37caee42a175288331
matheus062 <matheussilvestre3@gmail.com>
2022-05-03 23:04:08 -0300

    Cria Topbar e BottomBar.

--- app/src/main/java/com/example/petshopboyz_progmobile_univali/MainActivity.java
+++ app/src/main/java/com/example/petshopboyz_progmobile_univali/MainActivity.java
@@ -3,8 +3,7 @@ package com.example.petshopboyz_progmobile_univali;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
-
+import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -12,7 +11,16 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login);
+
+        Button buttonLogin = findViewById(R.id.buttonLogin);
+        buttonLogin.setOnClickListener (v -> setContentView(R.layout.home));
+
+
+
+
+
+
 
 
     }
