c027381fc58706532227c4d524e39cf71fe72085
matheus062 <matheussilvestre3@gmail.com>
2022-05-09 02:07:39 -0300

    Cria view de agendamentos dos pets.

--- app/src/main/java/com/example/petshopboyz_progmobile_univali/MainActivity.java
+++ app/src/main/java/com/example/petshopboyz_progmobile_univali/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -16,12 +17,10 @@ public class MainActivity extends AppCompatActivity {
         Button buttonLogin = findViewById(R.id.buttonLogin);
         buttonLogin.setOnClickListener (v -> setContentView(R.layout.home));
 
-
-
-
-
-
-
+//        ImageButton buttonSched = (findViewById(R.id.calendarButton) != null) ? findViewById(R.id.calendarButton) : null;
+//        if(buttonSched != null){
+//            buttonSched.setOnClickListener (v -> setContentView(R.layout.scheduling));
+//        }
 
     }
 }
